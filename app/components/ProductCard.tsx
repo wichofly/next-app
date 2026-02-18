c2a5f252e5e65fd10fa844c8688d23f6ec7f1428
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-18 14:35:06 +0100

    Add ProductCard component and integrate it into the Home page - "use client" is made to become a client components

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('Clicked')}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
