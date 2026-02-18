eba00bf27747e5d53695ba224a8195ad1d55330e
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-18 16:03:50 +0100

    Refactor ProductCard styling and update globals.css to use @import for Tailwind CSS

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import AddToCart from './AddToCart';
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl rounded-lg hover:bg-sky-500 transition-colors">
       <AddToCart />
     </div>
   );
