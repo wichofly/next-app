9275f76cde6913172b749260a2fff5a09e81131d
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-18 17:37:27 +0100

    Implement UserTable component and refactor UsersPage to utilize it - basic of routing inside of users

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,36 @@
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  const res = await fetch(process.env.URL_USERS!, { cache: 'no-store' });
+  const users: User[] = await res.json();
+
+  return (
+    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 mt-2">
+      <table className="table">
+        <thead className="bg-base-200">
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTable;
