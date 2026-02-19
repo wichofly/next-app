50cecd82ef8e3ff89fef577e92eba8c1623ff915
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-19 23:00:23 +0100

    Add POST API route for creating a new user with validation

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,14 @@ export const GET = () => {
     { id: 2, name: 'Paul' },
   ]);
 };
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+
+  // Validate the request body
+  if (!body.name)
+    return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+
+  // Save the data to a db
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+};
