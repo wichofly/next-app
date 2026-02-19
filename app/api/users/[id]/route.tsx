6998cb4f589d62dea62d83c20fe39d1d374574d9
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-19 15:47:08 +0100

    Add API route for fetching user data by ID with 404 handling

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+interface GetProps {
+  params: Promise<{ id: number }>;
+}
+
+export const GET = async (request: NextRequest, { params }: GetProps) => {
+  const { id } = await params;
+
+  // Fetch data from a db
+  // If not found, return a 404 error
+  // Else return data
+  // Make a rule and say if id is greater than 10, we are going to return a 404 error, otherwise we will return the data
+
+  if (id > 10)
+    return NextResponse.json({ message: 'User not found' }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: 'Jesus' });
+};
