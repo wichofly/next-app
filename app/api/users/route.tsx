5fb72099661f85578db0b4f7ae8329654f65752b
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-19 23:26:12 +0100

    Add PUT API route for updating user data with validation

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -17,3 +17,19 @@ export const POST = async (request: NextRequest) => {
   // Save the data to a db
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 };
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: number } },
+) => {
+  const { id } = params;
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+
+  if (id > 10)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+};
