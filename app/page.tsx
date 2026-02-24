b756924689d8889009f0f8b27a180c627a6fc274
Mauricio Erazo <antonierazo13@hotmail.com>
2026-02-24 22:05:04 +0100

    feat: refactor authentication handling and update homepage to display user name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import ProductCard from './components/ProductCard';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello Jesus!</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
