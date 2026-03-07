b7f88a6f9874a221c8514c6f238f30598a41edab
Mauricio Erazo <antonierazo13@hotmail.com>
2026-03-07 15:05:03 +0100

    feat: enhance WelcomeTemplate with Tailwind styling for improved layout

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -5,18 +5,23 @@ import {
   Link,
   Preview,
   Text,
+  Tailwind,
 } from '@react-email/components';
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome to our service!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://www.youtube.com">Youtube</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-gray-100 font-sans p-6">
+          <Container>
+            <Text className="text-lg text-semibold text-gray-800">
+              Hello {name}
+            </Text>
+            <Link href="https://www.youtube.com">Youtube</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
